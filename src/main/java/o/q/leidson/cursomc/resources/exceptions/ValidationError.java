4400bf342f3b40c5032d9e6f45dd1e5f9f1a8069
Leidson Oliveira <o.q.leidson@gmail.com>
2020-09-06 16:14:22 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/o/q/leidson/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package o.q.leidson.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> erros = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErros() {
+		return erros;
+	}
+
+	public void addError(String fieldNome, String message) {
+		erros.add(new FieldMessage(fieldNome, message));
+	}
+
+}
