8d183567ae3d8ba1b85491c34424fb2e48109772
Leidson Oliveira <o.q.leidson@gmail.com>
2020-09-08 21:49:11 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/o/q/leidson/cursomc/services/ClienteService.java
+++ src/main/java/o/q/leidson/cursomc/services/ClienteService.java
@@ -55,7 +55,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir o cliente porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir o cliente porque há pedidos relacionados a ele");
 
 		}
 	}
