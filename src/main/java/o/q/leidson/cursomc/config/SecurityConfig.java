73f3e3f74f078c4fa46f78bd6efb74cfd205952f
Leidson Oliveira <o.q.leidson@gmail.com>
2020-11-22 15:10:48 -0300

    Implementando autorizacao

--- src/main/java/o/q/leidson/cursomc/config/SecurityConfig.java
+++ src/main/java/o/q/leidson/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import o.q.leidson.cursomc.security.JWTAuthenticationFilter;
+import o.q.leidson.cursomc.security.JWTAuthorizationFilter;
 import o.q.leidson.cursomc.security.JWTUtil;
 
 @Configuration
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
