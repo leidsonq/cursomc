dc793c0a409484fc7e1c60ce1f136260c3c008c5
Leidson Oliveira <o.q.leidson@gmail.com>
2020-11-23 21:38:05 -0300

    Esqueci a senha

--- src/main/java/o/q/leidson/cursomc/services/EmailService.java
+++ src/main/java/o/q/leidson/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package o.q.leidson.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import o.q.leidson.cursomc.domain.Cliente;
 import o.q.leidson.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail (Pedido obj);
 	
 	void sendEmail (SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
